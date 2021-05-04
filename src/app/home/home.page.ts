b56c102339d728c9775f2353f8bbf6970ba57d9a
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-05-04 14:48:41 +0200

    Setup barcode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,43 +7,37 @@ import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-sca
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  scannedData: any;
-  encodedData: '';
-  encodeData: any;
-  inputData: any;
-  constructor(private barcodeScanner: BarcodeScanner) {
+  encodedData: any;
+  scannedBarCode: {};
+  barcodeScannerOptions: BarcodeScannerOptions;
 
-  }
+  constructor(private scanner: BarcodeScanner) {
 
-  scanBarcode() {
-    const options: BarcodeScannerOptions = {
-      preferFrontCamera: false,
-      showFlipCameraButton: true,
+    this.encodedData = "Programming isn't about what you know";
+    
+    this.barcodeScannerOptions = {
       showTorchButton: true,
-      torchOn: false,
-      prompt: 'Place a barcode inside the scan area',
-      resultDisplayDuration: 500,
-      formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
-      orientation: 'portrait',
+      showFlipCameraButton: true
     };
-
-    this.barcodeScanner.scan(options).then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-      this.scannedData = barcodeData;
-
-    }).catch(err => {
-      console.log('Error', err);
-    });
+    
   }
 
-  createBarcode() {
-    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, this.inputData).then((encodedData) => {
-      console.log(encodedData);
-      this.encodedData = encodedData;
-    }, (err) => {
-      console.log('Error occured : ' + err);
-    });
+  scanBRcode() {
+    this.scanner.scan().then(res => {
+        this.scannedBarCode = res;
+      }).catch(err => {
+        alert(err);
+      });
   }
 
-
+  generateBarCode() {
+    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodedData).then(
+        res => {
+          alert(res);
+          this.encodedData = res;
+        }, error => {
+          alert(error);
+        }
+      );
+  }
 }
