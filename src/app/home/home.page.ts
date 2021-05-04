08c9e5d46ecc5857248b60f459a2a4db6fd3262b
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-05-04 11:20:06 +0200

    checkout commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scannedData: any;
+  encodedData: '';
+  encodeData: any;
+  inputData: any;
+  constructor(private barcodeScanner: BarcodeScanner) {
 
-  constructor() {}
+  }
+
+  scanBarcode() {
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Place a barcode inside the scan area',
+      resultDisplayDuration: 500,
+      formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+      orientation: 'portrait',
+    };
+
+    this.barcodeScanner.scan(options).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.scannedData = barcodeData;
+
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+
+  createBarcode() {
+    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, this.inputData).then((encodedData) => {
+      console.log(encodedData);
+      this.encodedData = encodedData;
+    }, (err) => {
+      console.log('Error occured : ' + err);
+    });
+  }
+
 
 }
