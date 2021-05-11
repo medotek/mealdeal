df600e7d7b08684ba5406c4ce3e7c8d185840633
Enzo Tamayo <chonchon3007@gmail.com>
2021-05-11 08:24:10 +0200

    Nettoyage typescript homepage, autres...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,13 @@
 import {DealPrototype} from './../class/deal-prototype';
 import {Deal} from './../interfaces/deal';
 import {DaoService} from './../services/dao.service';
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner/ngx';
 import {HomeService} from '../services/home.service';
 import {Welcome as API} from '../interfaces/welcome';
 import {Product} from '../interfaces/product';
-import {FirebaseX} from '@ionic-native/firebase-x/ngx';
-import {Platform} from '@ionic/angular';
-import { HTTP, HTTPResponse } from '@ionic-native/http/ngx';
-import {HttpHeaders} from '@angular/common/http';
-import {DealService} from "../entity/deal.service";
-import {Router} from '@angular/router';
+import {FirebaseX} from "@ionic-native/firebase-x/ngx";
+import { Platform } from '@ionic/angular';
 
 
 @Component({
@@ -19,110 +15,71 @@ import {Router} from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
   encodedData: any;
   scannedBarCode: string;
   barcodeScannerOptions: BarcodeScannerOptions;
   public response: API;
   public monProduit: Product;
-  public isUserLoggedIn: string;
 
-  Deals: any = [];
+  //state
+  public oui = false;
+  public isUserLoggedIn: boolean = false;
 
-  constructor(private scanner: BarcodeScanner, private homeService: HomeService, private router: Router,
-              private firebase: FirebaseX,
-              private dao: DaoService,
-              private platform: Platform,
-              private http: HTTP,
-              private dealService: DealService
-              ) {
-
+  constructor(private platform: Platform,
+              private scanner: BarcodeScanner, 
+              private homeService: HomeService,
+              private firebase: FirebaseX) {
 
     this.encodedData = 'Programming isn\'t about what you know';
+
     this.barcodeScannerOptions = {
       showTorchButton: true,
       showFlipCameraButton: true
     };
-    this.platform.ready().then(() =>
+    
+    platform.ready().then(() => {
       this.firebase.setLanguageCode('fr').then(r => console.log(r))
-    );
+    }).then();
+
     this.homeService.searchProduct('5410041040807').subscribe((data: API) => {
       this.response = data;
       // récupère uniquement le tableau product
       console.log(this.response.product);
-    });
+
+    })
   }
 
   ngOnInit() {
     this.isUserLogged();
-    this.getAllDeals();
   }
 
   createAccount() {
-    this.platform.ready().then(() =>
-        this.firebase.createUserWithEmailAndPassword('test@test.fr', 'testtest').then(r =>
-          console.log(r)
-          //execute db queries
-        )
-      // FirebasePlugin.authenticateUserWithEmailAndPassword(email, password, function(credential) {
-      //   console.log("Successfully authenticated with email/password");
-      //   FirebasePlugin.reauthenticateWithCredential(credential, function() {
-      //     console.log("Successfully re-authenticated");
-      //   }, function(error) {
-      //     console.error("Failed to re-authenticate", error);
-      //   });
-      //   // User is now signed in
-      // }, function(error) {
-      //   console.error("Failed to authenticate with email/password", error);
-      // });
-    );
-  }
-
-  getAllDeals() {
-    this.platform.ready().then(() => {
-      this.dealService.getDealList().subscribe((res) => {
-        console.log(res)
-        this.Deals = res;
-      })
-    });
-  }
-
-  authenticateWithEmail() {
-    this.platform.ready().then(r =>
-      this.firebase.signInUserWithEmailAndPassword('test@test.fr', 'testtest').then(() => {
-          this.isUserLogged();
-          console.log(this.isUserLoggedIn);
-        }
-      )
-    );
-  }
-
-  logout() {
-    this.platform.ready().then(() =>
-      this.firebase.signOutUser().then(r => {
-        if (r) {
-          console.log('logged out ' + r);
-          this.router.navigate(['/connexion']);
-        } else {
-          console.log('not logged out' + r);
-        }
-      })
-    );
-
-    this.isUserLogged();
-    console.log(this.isUserLoggedIn);
+    this.firebase.createUserWithEmailAndPassword('test@test.fr', 'testtest').then(r => console.log(r))
+    // FirebasePlugin.authenticateUserWithEmailAndPassword(email, password, function(credential) {
+    //   console.log("Successfully authenticated with email/password");
+    //   FirebasePlugin.reauthenticateWithCredential(credential, function() {
+    //     console.log("Successfully re-authenticated");
+    //   }, function(error) {
+    //     console.error("Failed to re-authenticate", error);
+    //   });
+    //   // User is now signed in
+    // }, function(error) {
+    //   console.error("Failed to authenticate with email/password", error);
+    // });
   }
 
   isUserLogged() {
-    this.platform.ready().then(() =>
+    this.platform.ready().then(() => {
       this.firebase.isUserSignedIn().then(r => {
         if (r) {
-          this.isUserLoggedIn = 'connecté';
+          this.isUserLoggedIn = true;
         } else {
-          this.isUserLoggedIn = 'non connecté';
+          this.isUserLoggedIn = false;
         }
       })
-    );
+    })
+  
   }
 
   scanBRcode() {
@@ -132,6 +89,10 @@ export class HomePage implements OnInit {
       this.homeService.searchProduct(this.scannedBarCode).subscribe((data: API) => {
         this.response = data;
         this.monProduit = this.response.product;
+        this.oui = true;
+      });
+      this.homeService.searchProduct(this.scannedBarCode).subscribe(res => {
+        console.log(res);
       });
     });
   }
