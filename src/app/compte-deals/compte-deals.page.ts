767b1b15d125585b1af4b646dcb074a4c85d4998
Hugo Murienne <hugo.murienne@gmail.com>
2021-05-11 23:59:19 +0200

    get user deals

--- src/app/compte-deals/compte-deals.page.ts
+++ src/app/compte-deals/compte-deals.page.ts
@@ -1,3 +1,8 @@
+import { Platform } from '@ionic/angular';
+import { FirebaseUser } from '@ionic-native/firebase-x/ngx';
+import { AuthenticationService } from './../services/authentication.service';
+import { DealService } from './../entity/deal.service';
+import { Deal } from './../entity/deal';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -8,9 +13,44 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class CompteDealsPage implements OnInit {
 
-  constructor(private router: Router,) { }
+  private uid: string;
+  private deals: Deal[] = [];
+
+  constructor(private router: Router,
+              private platform: Platform,
+              private dealService: DealService,
+              private authService: AuthenticationService) {}
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.fetchCurrentUser();
+    this.fetchDeals();
+  }
+
+  getDeals() {
+    return this.deals;
+  }
+
+  fetchDeals() {
+    this.platform.ready().then(() => {
+      this.dealService.getDealbyAuthor(this.uid).subscribe((data) => {
+        this.deals = data;
+        console.log('Deals : '+this.deals);
+      });
+    });
+
+  }
+
+  fetchCurrentUser() {
+    this.authService.getCurrentUser()
+      .then((firebaseUser: FirebaseUser) => {
+        this.uid = firebaseUser.uid;
+        console.log('uid:'+this.uid);
+      }).catch((error) => {
+      alert(error);
+    });
   }
 
   addDeals(){
