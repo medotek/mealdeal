02e71752179975b4166ccdc0589a921bba48c4f2
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-05-11 10:02:34 +0200

    Linked Mongodb Model with FirebaseAuth

---
+++ src/app/entity/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import {User} from "./user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+    addUser(user: User): Observable<any> {
+    return this.http.post<User>('http://gudako.club:3001/api/user/create', user, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<User>('Add Deal'))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+}
